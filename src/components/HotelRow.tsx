0cb01bd000887ea9f8c1130e43d90aabe886907e
DrMerder <drmerder@yandex.com>
2024-06-04 17:03:03 +0500

    save

--- src/components/HotelRow.tsx
+++ src/components/HotelRow.tsx
@@ -1,11 +1,13 @@
 import {
   ArrowBackIos,
   ArrowForwardIos,
+  Check,
   Circle,
   Favorite,
   FlightLand,
   FlightTakeoff,
   Home,
+  Luggage,
   Wifi,
 } from "@mui/icons-material";
 import {
@@ -20,6 +22,7 @@ import {
   Button,
   CardOverflow,
   Sheet,
+  Divider,
 } from "@mui/joy";
 
 import { SetStateAction, useState } from "react";
@@ -59,7 +62,7 @@ const HotelRow = () => {
     "Телевизор",
     "Телефон",
   ];
-  const [opened,setOpened] = React.useState(false)
+  const [opened, setOpened] = React.useState(false);
   return (
     <>
       <Card
@@ -183,7 +186,9 @@ const HotelRow = () => {
                 sx={{ borderRadius: "99px", width: 20, height: 20 }}
                 variant="soft"
                 color="primary"
-                onClick={()=>{setOpened(true)}}
+                onClick={() => {
+                  setOpened(true);
+                }}
               >
                 <ArrowForwardIos />
               </IconButton>
@@ -191,102 +196,404 @@ const HotelRow = () => {
           </Stack>
         </CardContent>
       </Card>
-      <Sheet variant="soft" sx={{p:1, borderRadius: 10, mb:2}}>
-      <Card
-        variant="plain"
-        sx={{
-          margin: "auto",
-          boxShadow: 3,
-          p: 1,
-          m: 1,
-        }}
-      >
-        <CardContent>
-          <Stack direction="row" justifyContent="space-between">
-            <Stack>
-              <Typography level="body-sm">
-                07 июня (пт) - 15 июня (сб)
-              </Typography>
-              <Typography level="body-sm">8 ночей</Typography>
-            </Stack>
-            <Stack>
-              <Typography level="body-sm">
-                AI — Завтраки, обеды, ужины + напитки
-              </Typography>
-              <Typography level="body-sm">
-                DBL — Superior Garden View Room
-              </Typography>
-            </Stack>
+      <Sheet variant="soft" sx={{ p: 1, borderRadius: 10, mb: 2 }}>
+        <Card
+          variant="plain"
+          sx={{
+            margin: "auto",
+            boxShadow: 3,
+            p: 1,
+            m: 1,
+          }}
+        >
+          <CardContent>
+            <Stack direction="row" justifyContent="space-between">
+              <Stack>
+                <Typography level="body-sm">
+                  07 июня (пт) - 15 июня (сб)
+                </Typography>
+                <Typography level="body-sm">8 ночей</Typography>
+              </Stack>
+              <Stack>
+                <Typography level="body-sm">
+                  AI — Завтраки, обеды, ужины + напитки
+                </Typography>
+                <Typography level="body-sm">
+                  DBL — Superior Garden View Room
+                </Typography>
+              </Stack>
 
-            <Stack direction="row" gap={2}>
-              <Stack direction="column" gap={1}>
-                <Home fontSize="small" />
-                <Circle fontSize="small" sx={{ color: "orange" }} />
+              <Stack direction="row" gap={2}>
+                <Stack direction="column" gap={1}>
+                  <Home fontSize="small" />
+                  <Circle fontSize="small" sx={{ color: "orange" }} />
+                </Stack>
+                <Stack direction="column" gap={1}>
+                  <FlightTakeoff fontSize="small" />
+                  <Circle fontSize="small" sx={{ color: "orange" }} />
+                </Stack>
+                <Stack direction="column" gap={1}>
+                  <FlightLand fontSize="small" />
+                  <Circle fontSize="small" sx={{ color: "orange" }} />
+                </Stack>
               </Stack>
-              <Stack direction="column" gap={1}>
-              <FlightTakeoff fontSize="small" />
-                <Circle fontSize="small" sx={{ color: "orange" }} />
-              </Stack> 
-              <Stack direction="column" gap={1}>
-              <FlightLand fontSize="small" />
-                <Circle fontSize="small" sx={{ color: "orange" }} />
-              </Stack>
-            </Stack>
-           
-            <Button variant="outlined">264 854 T</Button>
-          </Stack>
-        </CardContent>
-      </Card>
-      <Card
-        variant="plain"
-        sx={{
-          margin: "auto",
-          boxShadow: 3,
-          p: 1,
-          m: 1,
-        }}
-      >
-        <CardContent>
-          <Stack direction="row" justifyContent="space-between">
-            <Stack>
-              <Typography level="body-sm">
-                07 июня (пт) - 15 июня (сб)
-              </Typography>
-              <Typography level="body-sm">8 ночей</Typography>
-            </Stack>
-            <Stack>
-              <Typography level="body-sm">
-                AI — Завтраки, обеды, ужины + напитки
-              </Typography>
-              <Typography level="body-sm">
-                DBL — Superior Garden View Room
-              </Typography>
-            </Stack>
 
-            <Stack direction="row" gap={2}>
-              <Stack direction="column" gap={1}>
-                <Home fontSize="small" />
-                <Circle fontSize="small" sx={{ color: "orange" }} />
-              </Stack>
-              <Stack direction="column" gap={1}>
-              <FlightTakeoff fontSize="small" />
-                <Circle fontSize="small" sx={{ color: "orange" }} />
-              </Stack> 
-              <Stack direction="column" gap={1}>
-              <FlightLand fontSize="small" />
-                <Circle fontSize="small" sx={{ color: "orange" }} />
-              </Stack>
+              <Button variant="outlined">264 854 T</Button>
             </Stack>
-           
-            <Button variant="outlined">264 854 T</Button>
-          </Stack>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+        <Card
+          variant="plain"
+          sx={{
+            margin: "auto",
+            boxShadow: 3,
+            p: 1,
+            m: 1,
+          }}
+        >
+          <CardContent>
+            <Stack direction="row" justifyContent="space-between">
+              <Stack>
+                <Typography level="body-sm">
+                  07 июня (пт) - 15 июня (сб)
+                </Typography>
+                <Typography level="body-sm">8 ночей</Typography>
+              </Stack>
+              <Stack>
+                <Typography level="body-sm">
+                  AI — Завтраки, обеды, ужины + напитки
+                </Typography>
+                <Typography level="body-sm">
+                  DBL — Superior Garden View Room
+                </Typography>
+              </Stack>
+
+              <Stack direction="row" gap={2}>
+                <Stack direction="column" gap={1}>
+                  <Home fontSize="small" />
+                  <Circle fontSize="small" sx={{ color: "orange" }} />
+                </Stack>
+                <Stack direction="column" gap={1}>
+                  <FlightTakeoff fontSize="small" />
+                  <Circle fontSize="small" sx={{ color: "orange" }} />
+                </Stack>
+                <Stack direction="column" gap={1}>
+                  <FlightLand fontSize="small" />
+                  <Circle fontSize="small" sx={{ color: "orange" }} />
+                </Stack>
+              </Stack>
+
+              <Button variant="outlined">264 854 T</Button>
+            </Stack>
+          </CardContent>
+        </Card>
       </Sheet>
-      <Sheet variant="soft" sx={{p:1, borderRadius: 10, mb:2}}>
-        
+      <Stack>
+        <Box sx={{ display: "flex", px: 4 }}>
+          <Typography
+            sx={{ borderRadius: "4px 4px 0 0", px: 1 }}
+            color="primary"
+            variant="solid"
+            level="body-xs"
+          >
+            Дешевый
+          </Typography>
+        </Box>
+
+        <Sheet
+          variant="soft"
+          sx={{
+            p: 1,
+            borderRadius: 10,
+            mb: 2,
+            border: "2px solid var(--joy-palette-primary-outlinedColor)",
+          }}
+        >
+          <Stack direction="row" gap={1}>
+            <Stack gap={1}>
+              <Stack
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 4,
+                  p: 2,
+                  backgroundColor: "#fff",
+                  borderRadius: 8,
+                  alignItems: "center",
+                  height: 80,
+                }}
+              >
+                <Box>
+                  <FlightTakeoff />
+                </Box>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack justifyContent="center" gap={0.5}>
+                  <Typography level="body-xs"> всего 9ч 15мин</Typography>
+                  <Stack direction="row">
+                    <Chip variant="soft" color="primary" size="sm">
+                      SVO
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      KZN
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      AST
+                    </Chip>
+                  </Stack>
+                  <Chip variant="solid" color="primary" size="sm">
+                    Без пересадок
+                  </Chip>
+                </Stack>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack>
+                  <Typography level="h3">Pobeda+</Typography>
+                  <Typography level="body-xs"> Рейс № 6895</Typography>
+                  <Typography level="body-xs"> ECONOM</Typography>
+                </Stack>
+                <Stack>
+                  <Luggage fontSize="large" color="error" />
+                  <Typography level="body-xs">Включена ручная кладь</Typography>
+                </Stack>
+              </Stack>
+              <Stack
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 4,
+                  p: 2,
+                  backgroundColor: "#fff",
+                  borderRadius: 8,
+                  alignItems: "center",
+                  height: 80,
+                }}
+              >
+                <Box>
+                  <FlightTakeoff />
+                </Box>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack justifyContent="center" gap={0.5}>
+                  <Typography level="body-xs"> всего 9ч 15мин</Typography>
+                  <Stack direction="row">
+                    <Chip variant="soft" color="primary" size="sm">
+                      SVO
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      KZN
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      AST
+                    </Chip>
+                  </Stack>
+                  <Chip variant="solid" color="primary" size="sm">
+                    Без пересадок
+                  </Chip>
+                </Stack>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack>
+                  <Typography level="h3">Pobeda+</Typography>
+                  <Typography level="body-xs"> Рейс № 6895</Typography>
+                  <Typography level="body-xs"> ECONOM</Typography>
+                </Stack>
+                <Stack>
+                  <Luggage fontSize="large" color="error" />
+
+                  <Typography level="body-xs">Включена ручная кладь</Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+            <Stack
+              sx={{ flex: 1, backgroundColor: "#fff", p: 1, borderRadius: 8 }}
+            >
+              <Stack flex={1} direction="row" justifyContent="space-evenly">
+                <Stack alignItems="flex-end">
+                  <Typography level="body-md" color="primary" fontWeight={700}>
+                    {" "}
+                    ДОПЛАТА
+                  </Typography>
+                  <Typography level="h2"> 0 Р</Typography>
+                </Stack>
+
+                <Stack alignItems="flex-end">
+                  <Typography level="body-md" color="primary" fontWeight={700}>
+                    {" "}
+                    ЦЕНА ТУРА
+                  </Typography>
+                  <Typography level="h2"> 300 000 Р</Typography>
+                </Stack>
+              </Stack>
+              <Button sx={{ width: "90%", margin: "auto" }}>
+                Забронировать
+              </Button>
+            </Stack>
+          </Stack>
         </Sheet>
-        <Description opened={opened} setOpened={setOpened}/>
+      </Stack>
+      <Stack>
+        <Box sx={{ display: "flex", px: 4 }}>
+       
+        </Box>
+
+        <Sheet
+          variant="soft"
+          sx={{
+            p: 1,
+            borderRadius: 10,
+            mb: 2,
+           
+          }}
+        >
+          <Stack direction="row" gap={1}>
+            <Stack gap={1}>
+              <Stack
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 4,
+                  p: 2,
+                  backgroundColor: "#fff",
+                  borderRadius: 8,
+                  alignItems: "center",
+                  height: 80,
+                }}
+              >
+                <Box>
+                  <FlightTakeoff />
+                </Box>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack justifyContent="center" gap={0.5}>
+                  <Typography level="body-xs"> всего 9ч 15мин</Typography>
+                  <Stack direction="row">
+                    <Chip variant="soft" color="primary" size="sm">
+                      SVO
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      KZN
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      AST
+                    </Chip>
+                  </Stack>
+                  <Chip variant="solid" color="primary" size="sm">
+                    Без пересадок
+                  </Chip>
+                </Stack>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack>
+                  <Typography level="h3">Pobeda+</Typography>
+                  <Typography level="body-xs"> Рейс № 6895</Typography>
+                  <Typography level="body-xs"> ECONOM</Typography>
+                </Stack>
+                <Stack>
+                  <Luggage fontSize="large" color="error" />
+                  <Typography level="body-xs">Включена ручная кладь</Typography>
+                </Stack>
+              </Stack>
+              <Stack
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 4,
+                  p: 2,
+                  backgroundColor: "#fff",
+                  borderRadius: 8,
+                  alignItems: "center",
+                  height: 80,
+                }}
+              >
+                <Box>
+                  <FlightTakeoff />
+                </Box>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack justifyContent="center" gap={0.5}>
+                  <Typography level="body-xs"> всего 9ч 15мин</Typography>
+                  <Stack direction="row">
+                    <Chip variant="soft" color="primary" size="sm">
+                      SVO
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      KZN
+                    </Chip>
+                    <Chip variant="soft" color="primary" size="sm">
+                      AST
+                    </Chip>
+                  </Stack>
+                  <Chip variant="solid" color="primary" size="sm">
+                    Без пересадок
+                  </Chip>
+                </Stack>
+                <Stack>
+                  <Typography level="h2"> 22: 25</Typography>
+                  <Typography level="body-xs"> 31 мая, пт</Typography>
+                  <Typography level="body-xs"> Москва</Typography>
+                </Stack>
+                <Stack>
+                  <Typography level="h3">Pobeda+</Typography>
+                  <Typography level="body-xs"> Рейс № 6895</Typography>
+                  <Typography level="body-xs"> ECONOM</Typography>
+                </Stack>
+                <Stack>
+                  <Luggage fontSize="large" color="error" />
+
+                  <Typography level="body-xs">Включена ручная кладь</Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+            <Stack
+              sx={{ flex: 1, backgroundColor: "#fff", p: 1, borderRadius: 8 }}
+            >
+              <Stack flex={1} direction="row" justifyContent="space-evenly">
+                <Stack alignItems="flex-end">
+                  <Typography level="body-md" color="primary" fontWeight={700}>
+                    {" "}
+                    ДОПЛАТА
+                  </Typography>
+                  <Typography level="h2"> 0 Р</Typography>
+                </Stack>
+
+                <Stack alignItems="flex-end">
+                  <Typography level="body-md" color="primary" fontWeight={700}>
+                    {" "}
+                    ЦЕНА ТУРА
+                  </Typography>
+                  <Typography level="h2"> 300 000 Р</Typography>
+                </Stack>
+              </Stack>
+              <Button sx={{ width: "90%", margin: "auto" }}>
+                Забронировать
+              </Button>
+            </Stack>
+          </Stack>
+        </Sheet>
+      </Stack>
+      <Description opened={opened} setOpened={setOpened} />
     </>
   );
 };
